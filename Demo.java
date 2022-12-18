bcd03ebdd83903c43fd1c9d0a4c0b0744d312d6f
shubhambakal <120928793+shubhambakal@users.noreply.github.com>
2022-12-18 17:20:02 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    public static void main(String args [])
+    {
+      int a=10;
+      int b=20;
+      a=a+b; 30
+      b=a-b;
+      a=a-b;
+      System.out.println("a="+a+" b="+b)
+    }
+}
